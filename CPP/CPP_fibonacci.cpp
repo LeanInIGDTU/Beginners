bcb4e98ecec0cb0a00eb6ebdd97d11477a21b8da
Apoorva Dhawan <adhawan13@gmail.com>
2017-11-23 19:21:33 +0530

    corrected spelling errors

--- CPP/CPP_fibonacci.cpp
+++ CPP/CPP_fibonacci.cpp
@@ -5,7 +5,7 @@ int main()
 int i,no, first=0, second=1, next;
 first=0;
 second=1;
-cout<<"Enter nubmer of terms for Series: ";
+cout<<"Enter number of terms for Series: ";
 cin>>no;
 cout<<"Fibonacci series are: \n";
 for(i=0; i<no; i++)
