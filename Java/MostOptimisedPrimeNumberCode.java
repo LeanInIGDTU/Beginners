a346a16ceb7c94a818f14e1cd71e92af95076b89
Tanishq Malhotra <33938577+tanishq98@users.noreply.github.com>
2020-10-01 02:48:55 +0530

    Added most optimised Code to find N prime number
    I have added the most optimized code to find N prime Number by taking care of both Time and Space Complexities. 
    Time Complexity of this code is almost half of the other codes of prime number found online. 
    This code can be used in interviews and competitive programming for better performance of your Solution.

---
+++ Java/MostOptimisedPrimeNumberCode.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class MostOptimisedPrimeNumberCode{
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int low = scn.nextInt();
+    int high = scn.nextInt();
+    
+    for(int num = low; num <= high; num++){
+        int div = 2;
+        while(div * div <= num){
+            if(num % div == 0){
+                break;
+            }
+            div++;
+        }
+        
+        if(div * div > num){
+            System.out.println(num);
+        }
+    }
+ }
+}
